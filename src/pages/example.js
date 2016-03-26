82e9b336d8b28d7dc4db6ddb8580e4710063a00d
Rodney cox <rodneycox@Rodneys-MBP.home>
2016-03-26 17:56:46 -0600

    second commit

--- src/pages/example.js
+++ src/pages/example.js
@@ -5,3 +5,32 @@ import ReactDom from 'react-dom'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 import { RouteHandler, Link, Router, Route, IndexRoute } from 'react-router'
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
+
+let ExampleComponent = React.createClass({
+  getInitialState: function() {
+    return {
+      someArray: [],
+      someAttribute: ""
+    }
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: Constants.api_base_url + Constants.api_version + '/findings/web_encryption/' + localStorage.analysis_id,
+      success: (data) => {
+          this.setState({
+              findings: data.someArray,
+              someAttribute: data.someAttribute,
+            }, () => {
+          })
+        }
+      }})
+    },
+    render: function(){
+      return (
+        <p>hello world</p>
+      )
+    }
+});
+
+module.exports = ExampleComponent;
